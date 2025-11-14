4c6030ccb9333de797f46e6d7ed15b658cc0441f
mozrin <mozrin@gmail.com>
2025-11-14 05:56:27 +0000

    fixes after figuring out it was a Microsoft problem and not a James problem.

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -8,6 +8,12 @@
 # export postAttachCommand=true
 
 ##### Set Helpful Environment Variables
+#
+# You can use alias and bash function() in this file. It is truly the bash_aliases file
+# for this ubuntu image and will be copied in place and used for whenever you start a terminal.
+#
+# enhancement? consider mapping this or symlinking it in place so that a modification can happen 
+#              after the workstation is created and initialized.
 
 cp -f ${DEVLITE_CONTAINER_DEVCONTAINER_FOLDER}/.bash_aliases ~/.bash_aliases
 
